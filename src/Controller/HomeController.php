d3a387a92bca24ccbe5c23ec6e7a17cf06652859
Matisse VARLIN <44835151+mvarlin@users.noreply.github.com>
2024-10-02 11:42:54 +0200

    Edit files and folders

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,16 +9,11 @@ class HomeController extends AbstractController
 {
     #[Route(path: '/home', name: 'home_page')]
     public function home(): Response {
-        return $this->render(view: 'index.html.twig');
+        return $this->render(view: 'others/index.html.twig');
     }
 
     #[Route(path: '/defaulthome', name: 'default_home_page')]
     public function default(): Response {
-        return $this->render(view: 'index.html.twig');
-    }
-
-    #[Route(path: '/basehome', name: 'base_home_page')]
-    public function base(): Response {
-        return $this->render(view: 'base.html.twig');
+        return $this->render(view: 'others/index.html.twig');
     }
 }
