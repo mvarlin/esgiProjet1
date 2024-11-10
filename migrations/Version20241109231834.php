979e5821dc3aef5d976ae6ce4a4a4d7ba095ec18
Matisse VARLIN <44835151+mvarlin@users.noreply.github.com>
2024-11-10 01:43:46 +0100

    New migration: Drop and edit Database

--- migrations/Version20241109231834.php
+++ migrations/Version20241109231834.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241030234252 extends AbstractMigration
+final class Version20241109231834 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20241030234252 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('ALTER TABLE episode CHANGE title title VARCHAR(255) NOT NULL, CHANGE duration duration INT NOT NULL, CHANGE release_date release_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('ALTER TABLE episode CHANGE title title LONGTEXT NOT NULL, CHANGE duration duration TIME NOT NULL, CHANGE release_date release_date DATE NOT NULL');
     }
 }
