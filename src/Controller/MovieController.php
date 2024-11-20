9669a54765b45edd2bc71967dbe6b0db9674716c
Matisse VARLIN <44835151+mvarlin@users.noreply.github.com>
2024-11-21 00:43:03 +0100

    WIP : moovie lists from database

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Controller;
+
+use App\Entity\Media;
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -28,8 +32,10 @@ class MovieController extends AbstractController
     }
 
     #[Route(path: '/lists', name: 'movie_lists')]
-    public function list(): Response {
-        return $this->render(view: 'movie/lists.html.twig');
+    public function list(EntityManagerInterface $entityManager): Response {
+        $repository = $entityManager->getRepository(Movie::class);
+        $movies = $repository->findAll();
+        return $this->render(view: 'movie/lists.html.twig', parameters: ['movies' => $movies]);
     }
 
     
