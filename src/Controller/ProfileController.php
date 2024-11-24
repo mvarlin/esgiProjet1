aa00b26f77d4ba6b4ecee8e5d33349427551923f
Matisse VARLIN <44835151+mvarlin@users.noreply.github.com>
2024-11-24 01:29:39 +0100

    Update profile page with subscription from database

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Subscription;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,8 +16,10 @@ class ProfileController extends AbstractController
     }
     
     #[Route('/myprofile/subscriptions', name: 'subscription_page')]
-    public function subscribe(): Response {
-        return $this->render(view: 'profile/subscriptions.html.twig');
+    public function subscribe(EntityManagerInterface $entityManager): Response {
+        $repositorySubscription = $entityManager->getRepository(Subscription::class);
+        $subscriptions = $repositorySubscription->findAllSubscriptions();
+        return $this->render(view: 'profile/subscriptions.html.twig', parameters: ['subscriptions' => $subscriptions]);
     }
 
     #[Route('/myprofile/settings', name: 'settings_page')]
