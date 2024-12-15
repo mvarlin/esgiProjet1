8a30881018b2158b1954a6df29100e64bb130d21
Matisse VARLIN <44835151+mvarlin@users.noreply.github.com>
2024-12-15 23:57:17 +0100

    WIP: send reset Email

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\Expr\From;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Mailer\MailerInterface;
 
 class AuthController extends AbstractController
 {
@@ -17,12 +21,33 @@ class AuthController extends AbstractController
     // }
 
     #[Route(path: '/forgot', name: 'auth_forgot')]
-    public function forgot(Request $request, EntityManagerInterface $entityManager): Response {
-        $email = $request->get('_email');
+    public function forgot(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailerInterface): Response {
+        $userMail = $request->get('_email');
         $repositoryUser = $entityManager->getRepository(User::class);
-        $result = $repositoryUser->findOneBy(['email'=>$email]);
-        dump($result);
-        dump($repositoryUser);
+        $result = $repositoryUser->findOneBy(['email'=>$userMail]); //Check en base via l'email
+        
+        if(!is_null($result)){ // il existe
+            $uuid = Uuid::v7();
+            dump($uuid);
+            $result->setResetToken($uuid); //modifier le champs uuid pour ajouter le token
+            dump($result);
+            $entityManager->persist($result);
+            $entityManager->flush(); //push
+
+            $email = (new TemplatedEmail())
+                ->from('fabien@example.com')
+                ->to($userMail)
+                ->subject('Email de')
+                ->htmlTemplate('email/reset.html.twig')
+                ->context([
+                    'resetToken' => $uuid,
+                    'userMail' => $userMail,
+                ]);
+            $mailerInterface->send($email);
+        } else { // existe pas
+            $this->addFlash('success', 'Utilisateur existe pas');
+            dump("exist pas");
+        }
         return $this->render(view: 'auth/forgot.html.twig');
     }
 
