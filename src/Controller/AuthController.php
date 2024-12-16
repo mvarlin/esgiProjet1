ee2949a7faa0da4671be136bf0f2acec6afae478
Matisse VARLIN <44835151+mvarlin@users.noreply.github.com>
2024-12-16 22:45:50 +0100

    Config mailer / reset password

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Query\Expr\From;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AuthController extends AbstractController
 {
@@ -22,31 +22,37 @@ class AuthController extends AbstractController
 
     #[Route(path: '/forgot', name: 'auth_forgot')]
     public function forgot(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailerInterface): Response {
-        $userMail = $request->get('_email');
-        $repositoryUser = $entityManager->getRepository(User::class);
-        $result = $repositoryUser->findOneBy(['email'=>$userMail]); //Check en base via l'email
         
-        if(!is_null($result)){ // il existe
-            $uuid = Uuid::v7();
-            dump($uuid);
-            $result->setResetToken($uuid); //modifier le champs uuid pour ajouter le token
-            dump($result);
-            $entityManager->persist($result);
-            $entityManager->flush(); //push
+        // Check si c'est une méthode post
+        if($request->isMethod('post')){
+            
+            //Recup du mail dans le form & Check en base via l'email
+            $userMail = $request->get('_email'); 
+            $user = $entityManager->getRepository(User::class)->findOneBy(['email'=>$userMail]);
 
-            $email = (new TemplatedEmail())
-                ->from('fabien@example.com')
-                ->to($userMail)
-                ->subject('Email de')
-                ->htmlTemplate('email/reset.html.twig')
-                ->context([
-                    'resetToken' => $uuid,
-                    'userMail' => $userMail,
-                ]);
-            $mailerInterface->send($email);
-        } else { // existe pas
-            $this->addFlash('success', 'Utilisateur existe pas');
-            dump("exist pas");
+            // Si l'utilisateur existe en base
+            if(!is_null($user)){
+
+                // Création de l'uuid & Push en base
+                $user->setResetToken(Uuid::v7());
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                // Création de mail (Token + destinataire) & Envoi du mail
+                $email = (new TemplatedEmail())
+                    ->from('matisse.netflex@streemi.com')
+                    ->to($userMail)
+                    ->subject('Email de')
+                    ->htmlTemplate('email/reset.html.twig')
+                    ->context([
+                        'resetToken' => $user->getResetToken(),
+                        'userMail' => $userMail,
+                    ]);
+                $mailerInterface->send($email);         
+            } else {
+                // Message Flash dans la view 
+                $this->addFlash('error', 'L\'utilisateur n\'existe pas');
+            }
         }
         return $this->render(view: 'auth/forgot.html.twig');
     }
@@ -61,10 +67,37 @@ class AuthController extends AbstractController
         return $this->render(view: 'auth/register.html.twig');
     }
 
-    #[Route(path: '/reset', name: 'auth_reset')]
-    public function user(): Response {
-        return $this->render(view: 'auth/reset.html.twig');
-    }
+    #[Route(path: '/reset/{token}', name: 'auth_reset')]
+    public function user(UserPasswordHasherInterface $uPHI, EntityManagerInterface $entityManager, string $token, Request $request): Response {
+        
+        // Check en base via le token
+        $user = $entityManager->getRepository(User::class)->findOneBy(['resetToken'=>$token]); 
+        
+        // Check si c'est une méthode post & l'utilisateur existe (à modifier ?)
+        if ($request->isMethod('POST') && !is_null($user)) { 
 
-    
+            // Les mots de passe sont identiques
+            if ($request->get('password') === $request->get('repeat-password')) {
+
+                // Hash le new mdp & Le push en base
+                $newPassword = $request->get('password');
+                $newPasswordHash = $uPHI->hashPassword($user, $newPassword); 
+                $user->setPassword($newPasswordHash); 
+                $entityManager->persist($user);
+                $entityManager->flush(); 
+                
+                // Message de validation & Retour vers la page login
+                $this->addFlash('success', 'Votre nouveau mot de passe a été modifié correctement');
+                return $this->redirectToRoute('app_login');
+            } else {
+                // dump("mdp différent");
+                return $this->render(view: 'auth/reset.html.twig');
+            }
+        }
+        if(!is_null($user)){ // il existe
+            return $this->render(view: 'auth/reset.html.twig');
+        } else{
+            return $this->render(view: 'auth/reset.html.twig');
+        }
+    }
 }
